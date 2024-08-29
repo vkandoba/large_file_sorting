2260397ff4bbbd4b5491af9a0fec13c9c9d31f1a
Viktor Kandoba <vkandoba@gmail.com>
2024-08-29 21:32:49 +0200

    first impl

---
+++ LargeFileSorting/LargeFileSortingApp/IFileSortingService.cs
@@ -0,0 +1,44 @@
+namespace LargeFileSorting;
+
+public class LinePair : IComparable
+{
+    public string Number { get; set; }
+    public string String { get; set; }
+    
+    public int CompareTo(object? obj)
+    {
+        var other = obj as LinePair; // TODO: check when obj is null
+
+        var stringCompareResult = String.Compare(String, other?.String, StringComparison.Ordinal);
+        if (stringCompareResult != 0)
+            return stringCompareResult;
+        
+        return String.Compare(Number, other?.Number, StringComparison.Ordinal);
+    }
+
+    public string MakeString() => $"{Number}.{String}";
+}
+
+public interface IFileSortingService
+{
+    IEnumerable<string> Do(IEnumerable<string> lines);
+}
+
+public class FileSortingService : IFileSortingService
+{
+    public IEnumerable<string> Do(IEnumerable<string> lines)
+    {
+        var linePairs = ParseLines(lines).ToArray();
+        Array.Sort(linePairs);
+        return linePairs.Select(x => x.MakeString());
+    }
+
+    private IEnumerable<LinePair> ParseLines(IEnumerable<string> lines)
+    {
+        foreach (var line in lines)
+        {
+            var tokens = line.Split('.');
+            yield return new LinePair { Number = tokens[0], String = tokens[1] };
+        }
+    }
+}
---
+++ LargeFileSorting/LargeFileSortingApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.IO.Compression;
+
+using LargeFileSorting;
+
+var service = new FileSortingService();
+
+var lines = File.ReadLines("in.txt");
+
+var result = service.Do(lines);
+
+foreach (var line in result)
+{
+    Console.WriteLine(line);
+}
