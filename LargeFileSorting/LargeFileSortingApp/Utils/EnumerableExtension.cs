e7562191a4fa01649e1e11d943f38f1253162f79
Viktor Kandoba <vkandoba@gmail.com>
2024-08-30 21:57:04 +0200

    fix condition

--- LargeFileSorting/LargeFileSortingApp/Utils/EnumerableExtension.cs
+++ LargeFileSorting/LargeFileSortingApp/Utils/EnumerableExtension.cs
@@ -13,13 +13,15 @@ public static class EnumerableExtension
         long currentSize = 0;
         foreach (var line in lines)
         {
-            var strSize = line.Number.Length * 2;
-            var numberSize = line.String.Length * 2;
+            // TODO: not effient way to determinate size of string. May be replaced to mean size of file
+            var strSize = Encoding.UTF8.GetByteCount(line.String);
+            var numberSize = Encoding.UTF8.GetByteCount(line.Number);
             currentSize += strSize + numberSize;
             current.Add(line);
 
-            if (currentSize < chunkSizeB)
+            if (currentSize > chunkSizeB)
             {
+                // Console.WriteLine($"generate chunk of {current.Count} lines and {currentSize:n0} size");
                 yield return current.ToArray();
                 current.Clear();
                 currentSize = 0;
