97d409d173759104cc9be1b9943b1e84f58b3619
Viktor Kandoba <vkandoba@gmail.com>
2024-09-01 19:48:09 +0200

    Add CLI arguments

--- LargeFileSorting/LargeFileSortingApp/SortingMain.cs
+++ LargeFileSorting/LargeFileSortingApp/SortingMain.cs
@@ -5,31 +5,45 @@ using LargeFileSortingApp.SortingService;
 
 // TODO: handle exceptions
 // TODO: profile memory for 1 GB
+
+var inputFile = "in.txt";
+var outputFile = "out.txt";
+if (args.Length > 0)
+{
+    inputFile = args[0];
+    if (args.Length > 1)
+    {
+        outputFile = args[1];
+    }
+}
+
+#if DEBUG
+    Console.WriteLine($"Input: {inputFile}\nOutput: {outputFile}"); 
+#endif
+
 var totalWatch = System.Diagnostics.Stopwatch.StartNew();
 
 var service = new ChunkSortingService();
 
-var inFile = "/Users/vkandoba/data/test_1_gb_30_250";
-var textLines = File.ReadLines(inFile);
+var textLines = File.ReadLines(inputFile);
 var lines = textLines.Select(LineItem.Parse);
 var sortedLines = service.Sort(lines);
 var result = sortedLines.Select(l => l.Line);
 
-var outputFile = "out.txt";
 if (File.Exists(outputFile))
 {
     File.Delete(outputFile);
 }
 File.AppendAllLines(outputFile, result);
-
 var total_ms = totalWatch.ElapsedMilliseconds;
 
-Console.WriteLine($"total exec time: {total_ms / 1000.0:N2} sec.\n");
+#if DEBUG
+    Console.WriteLine($"Done. Exec time: {total_ms / 1000.0:N2} sec.\n");
+#endif
 
-var firstResult = File.ReadLines("out.txt").Take(10);
+var firstResult = File.ReadLines(outputFile).Take(10);
 
 foreach (var line in firstResult)
 {
     Console.WriteLine(line);
 }
-
